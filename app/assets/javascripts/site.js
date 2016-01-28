85eb47996593e04451a66dd043739512a48d3291
Jackson Chen <tiger2chen@gmail.com>
2016-01-27 20:56:56 -0700

    Finish auction form, add favicon

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     $('input#rememberme').prop("checked", $('paper-checkbox#rememberme').prop("checked"))
     $("#form").submit();
   })
+
+  $('paper-item.auctions').mouseover(function() {
+    $(this).css('background-color', '#EEE');
+  });
+
+  $('paper-item.auctions').mouseout(function() {
+    $(this).css('background-color', '#FFF');
+  })
 })
--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,4 +1,9 @@
 class AuctionsController < ApplicationController
+  before_action :authenticate_admin!, only: [:new, :create]
+  def show
+    @current_auction = Auction.find_by_id(params[:id])
+  end
+
   def new
     @auction = Auction.new
   end
@@ -7,6 +12,7 @@ class AuctionsController < ApplicationController
     @auction = Auction.new(auction_params)
 
     if @auction.save
+      @auction.admins << current_admin
       flash[:notice] = "Your auction has been made."
       redirect_to root_path
     else
