7a87303e6647777400275b718f7149b8451f8396
Jackson Chen <tiger2chen@gmail.com>
2016-02-06 12:09:30 -0700

    Enables edit and delete options for auction

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,5 +1,5 @@
 class AuctionsController < ApplicationController
-  before_action :authenticate_admin!, only: [:new, :create]
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :destroy]
   def show
     @current_auction = Auction.find_by_id(params[:id])
   end
@@ -16,8 +16,34 @@ class AuctionsController < ApplicationController
       flash[:notice] = "Your auction has been made."
       redirect_to root_path
     else
-      flash[:notice] = "These was an error while saving"
+      flash[:notice] = "These was an error while saving."
       render :new
+    end
+  end
+
+  def edit
+    @auction = Auction.find_by_id(params[:id])
+  end
+
+  def update
+    @auction = Auction.find_by_id(params[:id])
+    if @auction.update_attributes(auction_params)
+      flash[:notice] = "Your auction has been changed."
+      redirect_to auction_path
+    else
+      flash[:notice] = "These was an error while saving."
+      render :edit
+    end
+  end
+
+  def destroy
+    @auction = Auction.find_by_id(params[:id])
+    if @auction.destroy
+      flash[:notice] = "Auction deleted."
+      redirect_to root_path
+    else
+      flash[:error] = "Error when deleting auction."
+      render :show
     end
   end
 
