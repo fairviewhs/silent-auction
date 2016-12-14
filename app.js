30776ef20ae09c4716623938c6f04b87bd633973
DonoA <dallen@dallen.io>
2016-12-13 22:15:21 -0700

    fixed layout system

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+
+app.use(function(req,res,next){
+  res.locals.session = {};//req.session;
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
